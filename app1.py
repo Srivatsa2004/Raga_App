17bd700b61973c1110bf91124883e5f4eb4d4940
Srivatsa2004 <137084792+Srivatsa2004@users.noreply.github.com>
2025-03-05 00:20:32 +0530

    Update app1.py

--- app1.py
+++ app1.py
@@ -15,14 +15,12 @@ if uploaded_file is not None:
     onset_detect, onset_times = detect_onsets(y, sr,thresh)
     onset_frequencies = get_onset_frequencies(y, sr, onset_detect)
 
-    shruthis = {
-    ('C', 'Sa'): [261.63, 523.25], ('C#', 'R1'): [277.18, 554],
-    ('D', 'R2/G1'): [293.66, 587.33], ('D#', 'R3/G2'): [311.13, 622.25],
-    ('E', 'G3'): [329.63, 659.25], ('F', "M1"): [349.23, 698],
-    ('F#', "M2"): [369.99, 739.99, 1479], ('G', "P"): [392, 783],
-    ('G#', "D1"): [415.3, 830.61, 1661], ('A', "D2/N1"): [440, 880, 1760],
-    ('A#', "D3/N2"): [466.16, 932.68, 1864], ('B', "N3"): [246.94, 493.88, 987.77, 1975.5]
-    }
+    shruthis={('C','Sa'): [261.63,523.25], ('C#','R1'): [277.18,554.37],
+          ('D', 'R2/G1'): [293.66,587.33],  ('D#','R3/G2'): [311.13,622.25],
+          ('E','G3'): [329.63,659.25], ('F',"M1"): [349.23,698],
+          ('F#',"M2"): [369.99,739.99], ('G',"P"): [392,783],
+          ('G#',"D1"): [415.3,830.61],('A',"D2/N1"): [440,880],
+          ('A#',"D3/N2"): [466.16,932.68], ('B',"N3"): [246.94, 493.88, 987.77]}
     input_shruthi = st.text_input("Enter the shruthi (e.g., C, C#, D):")
     if input_shruthi:
         unique_swaras = get_shifted_swaras(onset_frequencies, shruthis, input_shruthi)

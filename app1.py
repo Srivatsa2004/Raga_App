d3cd73dc4e16c497d9d664dfb24a33934a867560
Srivatsa2004 <137084792+Srivatsa2004@users.noreply.github.com>
2025-03-04 23:45:22 +0530

    Update app1.py

--- app1.py
+++ app1.py
@@ -12,8 +12,8 @@ if uploaded_file is not None:
 
     y, sr = load_audio("temp.opus")
     y_clean = apply_noise_cancellation(y)
-    onset_detect, onset_times = detect_onsets(y_clean, sr,thresh)
-    onset_frequencies = get_onset_frequencies(y_clean, sr, onset_detect)
+    onset_detect, onset_times = detect_onsets(y, sr,thresh)
+    onset_frequencies = get_onset_frequencies(y, sr, onset_detect)
 
     shruthis = {
     ('C', 'Sa'): [261.63, 523.25], ('C#', 'R1'): [277.18, 554],

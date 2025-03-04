9b78b70f5f1b77ab6a52f0c223335824837ae6f1
Srivatsa2004 <137084792+Srivatsa2004@users.noreply.github.com>
2025-03-04 20:37:47 +0530

    Update my_funcs.py

--- my_funcs.py
+++ my_funcs.py
@@ -16,7 +16,7 @@ def apply_noise_cancellation(y, thresh):
 
 def detect_onsets(y, sr,thresh=0.02):
     y_clean = apply_noise_cancellation(y, thresh)
-    onset_detect = librosa.onset.onset_detect(y=y,sr=sr)
+    onset_detect = librosa.onset.onset_detect(y=y_clean,sr=sr)
     onset_times = librosa.frames_to_time(onset_detect, sr=sr)
     return onset_detect, onset_times
 

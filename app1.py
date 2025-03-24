d47ad6c588cf8575fa30fcc68529652e18fef40b
Srivatsa2004 <137084792+Srivatsa2004@users.noreply.github.com>
2025-03-24 09:49:08 +0530

    Update app1.py

--- app1.py
+++ app1.py
@@ -38,6 +38,6 @@ if uploaded_file is not None:
     #st.write("Onset Frequencies:", onset_frequencies)
     #st.write("Swaras:", unique_swaras)
     #st.write("Matched swaras:", matched_swaras)
-    st.write("Possible Ragas:", matched_ragas)
+    #st.write("Possible Ragas:", matched_ragas)
 else:
     st.write("Please enter a shruthi")

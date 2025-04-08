40a30fd9d796fd39d7c3d9a21ac1b3777562700b
Srivatsa2004 <137084792+Srivatsa2004@users.noreply.github.com>
2025-04-09 00:34:06 +0530

    Update app1.py

--- app1.py
+++ app1.py
@@ -5,7 +5,7 @@ from pathlib import Path
 from myfuncs1 import load_audio, apply_noise_cancellation, detect_onsets, plot_onsets, get_onset_frequencies, match_swaras, get_shifted_swaras, find_raga
 
 #bg code
-background_image_url = "pic.jpg"
+background_image_url = "https://github.com/Srivatsa2004/Raga_App/blob/b1cac7692c4ba7bfb9b93fccb07990a0535bc720/pic.jpg"
 
 st.markdown(
     f"""
